b4da14ce55296de1a04c11441b95c6fddee046a5
Edward1yadidLevi <142442624+Edward1yadidLevi@users.noreply.github.com>
2023-08-27 19:34:30 -0700

    .

---
+++ script.js
@@ -0,0 +1,158 @@
+const worddisplay = document.getElementById("word-display");
+const wrongLetter = document.getElementById("wrong-letter");
+const letter = document.querySelector(".letter");
+const winMessage = document.getElementById("message");
+const popUp = document.getElementById("popUp");
+const plyaAgainButton = document.getElementById("plyaAgainButton");
+const guessestext = document.getElementById("guesses-text");
+const wrongL = document.getElementById("wrongLetter");
+const bodyParts = document.querySelectorAll(".bodyParts");
+const bodyPartsGun = document.querySelectorAll(".bodyPartsGun");
+const randomline = document.getElementById("randomcondolence");
+const fat = document.querySelector(".fat");
+const cointaernotification = document.getAnimations("cointaer-notification");
+const hinttext = document.getElementById("hint-text");
+
+//radnom word and hint
+let { word, hint } = worldList[Math.floor(Math.random() * worldList.length)];
+
+let wordChhosen = word;
+
+// array for current letter or uncurrent
+const wrongletterArray = [];
+const correctletter = [];
+/// keyDown keyboard
+document.addEventListener("keydown", keyboardLetter);
+const lowerCaseRext = /[a-z]/;
+function keyboardLetter(event) {
+  if (event.key === event.key.toLowerCase() && event.key.match(lowerCaseRext)) {
+    const letterKey = event.key;
+    // conditon for only loewrCse
+    if (wordChhosen.includes(letterKey)) {
+      if (!correctletter.includes(letterKey)) {
+        // check if the letter pressed twice for push to awway only once
+        correctletter.push(letterKey);
+      }
+      showTheWorld();
+      UpdateBlessLine();
+    }
+    if (
+      !wrongletterArray.includes(letterKey) &&
+      !correctletter.includes(letterKey)
+    ) {
+      UpdateWrongLetter(letterKey);
+    }
+  }
+}
+
+hinttext.innerHTML = `question: ${hint} `;
+function showTheWorld() {
+  console.log(correctletter);
+  let li = "";
+  wordChhosen.split("").map((value) => {
+    li += ` <li class="letter">${
+      correctletter.includes(value) ? value : ""
+    }</li>`;
+  }); // check if the letter includs in the word and if yes the letter is visiblei if not the letter is disible
+  worddisplay.innerHTML = li;
+  const clearLetter = worddisplay.innerText.replace(/\n/g, "");
+  if (wordChhosen == clearLetter) {
+    winMessage.innerText =
+      "Congratulations on an amazing win!, because of you i'm alive🤣";
+    popUp.style.display = "flex";
+  }
+}
+
+// popUp notification
+
+function notification() {
+  const notification = document.getElementById("cointaer-notification");
+  notification.style.display = "flex";
+  notification.style.backgroundColor = "yellow";
+  setTimeout(() => {
+    notification.style.display = "none";
+  }, 2000);
+}
+
+// update letterUcorrect
+let count = 0;
+function UpdateWrongLetter(letterKey) {
+  wrongletterArray.push(letterKey); // push to array the wrong letter
+  wrongL.innerHTML = `wrong Letter:${wrongletterArray.join("")}`;
+  count = wrongletterArray.length;
+  guessestext.innerHTML = `Incorrect guesses:<b> ${count} / 5</b>`;
+  const condolenceChhosen =
+    condolence[Math.floor(Math.random() * condolence.length)];
+  randomline.innerHTML = `condolence:
+    <br/>
+  ${condolenceChhosen}`;
+
+  bodyParts.forEach((parts, index) => {
+    if (index < count) {
+      parts.style.display = "block";
+    } else {
+      parts.style.display = "none";
+    }
+  });
+
+  if (count == 5) {
+    setTimeout(() => {
+      winMessage.innerText = "Game over, man! Game over!, i'm dead🤣";
+      popUp.style.display = "flex";
+    }, 3000);
+    playAudio();
+    fat.style.stroke = "red";
+    const gun = document.getElementById("gun");
+    bodyPartsGun.forEach((value) => {
+      value.style.display = "block";
+    });
+    gun.style.display = "block";
+  }
+}
+
+//undape condolence line or bleesing line
+function UpdateBlessLine() {
+  const blessChhosen =
+    blessingLines[Math.floor(Math.random() * blessingLines.length)];
+  console.log(blessChhosen);
+  randomline.innerHTML = `blessLine:
+  <br/>
+  ${blessChhosen}`;
+}
+
+let sound = document.getElementById("mySound");
+function playAudio() {
+  sound.play();
+}
+
+// Restart game and play again
+plyaAgainButton.addEventListener("click", removeAllElements);
+//  Empty arrays
+function removeAllElements(event) {
+  popUp.style.display = "none";
+
+  correctletter.splice(0, correctletter.length);
+  wrongletterArray.splice(0, wrongletterArray.length);
+
+  console.log(correctletter, wrongletterArray);
+
+  UpdateWrongLetter();
+
+  clearAll();
+  let { word, hint } = worldList[Math.floor(Math.random() * worldList.length)];
+  wordChhosen = word;
+  hinttext.innerHTML = `question: ${hint} `;
+  showTheWorld();
+  console.log(wordChhosen, hint);
+}
+function clearAll() {
+  count == 0;
+  guessestext.innerHTML = `Incorrect guesses:<b> ${count - 1} / 5</b>`;
+  randomcondolence.innerText = "";
+  fat.style.display = "none";
+
+  bodyPartsGun.forEach((value) => {
+    value.style.display = "none";
+  });
+  gun.style.display = "none";
+}
---
+++ world-list.js
@@ -0,0 +1,52 @@
+const worldList = [
+  {
+    word: "harvest",
+    hint:
+      "The process of gathering mature crops, fruits, or plants from the fields, typically for consumption, sale, or storage. ",
+  },
+
+  { word: "bycycle", hint: "A human-powered vehicle with two wheels." },
+  {
+    word: "science",
+    hint:
+      " the systematic study of the structure and behaviour of the physical and natural world through observation and experiment, and technology is the application of scientific knowledge for practical purposes.  ",
+  },
+
+  {
+    word: "diamond",
+    hint:
+      " A naturally occurring crystalline carbon mineral, prized for its exceptional hardness, brilliance, and use in jewelry and industrial applications.",
+  },
+  {
+    word: "australia",
+    hint: "Which country do cities of Perth, Adelade & Brisbane belong to?",
+  },
+  {
+    word: "samsung",
+    hint: "What is the name of the biggest technology company in South Korea?",
+  },
+  {
+    word: "octagon",
+    hint: "What geometric shape is generally used for stop signs?",
+  },
+];
+
+const condolence = [
+  "Please accept my deepest condolences.😆",
+  "Never forget, you have friends who love you.😢",
+  "Sharing in your sorrow. With love and friendship.😱",
+  "Thinking of you, in this difficult time🤐",
+  "Thinking of you, in this difficult time🤐",
+];
+const blessingLines = [
+  "May your days be filled with joy and your heart with peace.",
+  "Wishing you love, laughter, and endless happiness.",
+  "May each step you take be guided by light and goodness.",
+  "Sending you warm thoughts and positive vibes for your journey.",
+  "May your dreams flourish and your spirit soar to new heights.",
+  "Blessings of health, strength, and abundant prosperity to you.",
+  "May your path be surrounded by love and your days be blessed.",
+  "Wishing you a life full of cherished moments and beautiful memories.",
+  "May your days be bright, your nights peaceful, and your heart content.",
+  "Sending you a cascade of blessings for today and all your tomorrows.",
+];
